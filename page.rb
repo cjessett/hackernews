414b04dccdf4d0c23a84811793ee6b8f86d4afd0
CJ Jessett <email": null>
2016-07-21 13:00:03 -0500

    add page class and runner code to display HN homepage nicely

--- page.rb
+++ page.rb
@@ -1,2 +1,25 @@
+
 class Page
+  attr_reader :document
+
+  def initialize(html_document)
+    @document = html_document
+  end
+
+  def title
+    document.css('title').text
+  end
+
+  def links
+    links = {}
+    document.css('a').each do |link|
+      links[link.text] = link.attributes['href'].value
+    end
+    links.select { |text, url| url[0..3] == 'http' }
+  end
+
+  def character_count
+    document.css('body').text.gsub(/\s+/,"").length
+  end
 end
+
--- runner.rb
+++ runner.rb
@@ -1,5 +1,31 @@
-require 'net/http'
+require 'httparty'
 require 'nokogiri'
+require 'pry'
 
 require_relative 'page'
 
+if ARGV.any?
+  url = ARGV.first
+else
+  url = 'https://news.ycombinator.com/'
+end
+
+response = HTTParty.get(url)
+
+parsed = Nokogiri.parse(response)
+
+page = Page.new(parsed)
+
+puts <<END
+#{page.title}
+#{url}
+
+Content length: #{page.character_count.to_s}.
+
+Links:
+END
+
+page.links.each do |text, url|
+  puts "- #{text}"
+  puts "  #{url}\n\n"
+end
